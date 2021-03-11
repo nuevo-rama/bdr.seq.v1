2e773d3c0a7cbff029e67d4599fb5ea9d71fa6a2
nuevo-rama <ramiromelero@gmail.com>
2021-03-11 09:39:45 -0300

    estructura de carpetas y archivos iniciales para MVC

---
+++ controllers/productosController.js
@@ -0,0 +1,40 @@
+//CONTROLADOR PARA ...funciones que se van a encargar de manejar a los productos
+const productosController = {
+
+    index: (req,res)=>{
+        res.render("./products/listadoProductos")
+    },
+
+    product: (req,res)=>{
+        res.render("./products/product")
+    },
+    
+    shoppingCart: (req,res)=>{
+        res.render("./products/ShoppingCart")
+    },
+
+    productFormNew: (req,res)=>{
+        res.render("./products/productFormNew")
+    },
+
+    productFormEdit: (req,res)=>{
+        res.render("./products/productFormEdit")
+    },
+
+    //----------------------------------------------
+
+    detalleProducto: function (req,res) {
+        res.send ("Bienvenidos al detalle de un producto" + req.params.idProducto)
+    },
+
+    detalleProductoComentario: function (req,res) {
+        if (req.params.idComentario==undefined) {
+            res.send ("Bienvenidos al detalle de un producto" + req.params.idProducto); 
+        } else {
+            res.send ("Bienvenidos al comentario del producto" + req.params.idProducto + 
+            "y estás en el comentario " + req.params.idComentario); 
+        }
+    }
+}
+
+module.exports=productosController; //se exportará hacia routes
